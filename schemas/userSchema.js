77e9b1c7e9c9060620a87526200a94b2f77f76ce
Moin <cmoinahmed@gmail.com>
2024-09-29 19:43:27 +0530

    initial push

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -40,6 +40,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", async function (next) {
+  this.dateModified = new Date();
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("users", userSchema);
 
 export default User;
