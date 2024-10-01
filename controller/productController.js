877a855eaec386d8db7d9dc8619dd35c95073037
Hafeez Mulla <mahhmmedhafeezmulla@gmail.com>
2024-10-01 12:29:44 +0530

    New API in products

--- controller/productController.js
+++ controller/productController.js
@@ -68,10 +68,32 @@ export const updateProduct = asyncHandler(async (req, res) => {
 
 export const getAll = asyncHandler(async (req, res) => {
   try {
-    const productDoc = await Product.find({});
-
-    console.log(productDoc);
-    return res.status(200).json({ success: true, productDoc });
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+    const sortField = req.query.sortField || "name";
+    const sortOrder = req.query.sortOrder || "asc";
+    const sort = {};
+    sort[sortField] = sortOrder === "asc" ? 1 : -1;
+    const startIndex = (page - 1) * pageSize;
+    const totalDocuments = await Product.countDocuments();
+    const totalPages = Math.ceil(totalDocuments / pageSize);
+    const productDoc = await Product.find({})
+      .sort(sort)
+      .skip(startIndex)
+      .limit(pageSize)
+      .exec();
+    return res.status(200).json({
+      productDoc,
+      pagination: {
+        page,
+        pageSize,
+        totalPages,
+        totalDocuments,
+        hasNextPage: page < totalPages,
+        hasPrevPage: page > 1,
+      },
+      success: true,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ success: false, error });
@@ -99,4 +121,43 @@ export const getProductById = asyncHandler(async (req, res) => {
 });
 
 //getProductSoldCount
+export const getProductSoldCount = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const productDoc = await Product.findById(id).select("productSold");
+    if (!productDoc) {
+      console.log("Product not found");
+      return res.status(404).json({ success: false, msg: "Product not found" });
+    }
+    return res.status(200).json({
+      success: true,
+      productDoc,
+      ProductSold: Product.productSold,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, error });
+  }
+});
+
 //getGrossRevenueByProductId
+export const getProductRevenue = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const productDoc = await Product.findById(id).select("grossRevenue");
+    if (!productDoc) {
+      console.log("Product not found");
+      return res.status(404).json({ success: false, msg: "Product not found" });
+    }
+    return res.status(200).json({
+      success: true,
+      productDoc,
+      grossRevenue: Product.grossRevenue,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, error });
+  }
+});
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,8 @@ import {
   addProduct,
   getAll,
   getProductById,
+  getProductRevenue,
+  getProductSoldCount,
   updateProduct,
 } from "../controller/productController.js";
 
@@ -12,5 +14,7 @@ productRouter.route("/add").post(addProduct);
 productRouter.route("/updateProduct").put(updateProduct);
 productRouter.route("/getAll").get(getAll);
 productRouter.route("/getById/:productId").get(getProductById);
+productRouter.route("/getProductSoldCount/:id").get(getProductSoldCount);
+productRouter.route("/getProductRevenue/:id").get(getProductRevenue);
 
 export default productRouter;
